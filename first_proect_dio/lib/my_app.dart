2235fcd6816eeca74d52cf4dd040074e1428817e
Alex <alexbaukartjarschel@gmail.com>
2024-02-14 21:09:05 -0300

    n deu de fazer nada hj

--- first_proect_dio/lib/my_app.dart
+++ first_proect_dio/lib/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:first_proect_dio/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
+          textTheme: GoogleFonts.pacificoTextTheme(),
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
           useMaterial3: true,
         ),
--- first_proect_dio/lib/pages/home_page.dart
+++ first_proect_dio/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:first_proect_dio/service/random_number_creator.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,24 +11,43 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var generatedNumber = 0;
+  var clicks = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('App'),
+        title: Text(
+          'App',
+          style: GoogleFonts.pacifico(),
+        ),
       ),
-      body: Center(
-        child: Text(generatedNumber.toString()),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              " Random nnumber $generatedNumber",
+              style: GoogleFonts.acme(fontSize: 30),
+            ),
+          ),
+          Center(
+            child: Text(
+              "foi Clicado $clicks vezes",
+              style: GoogleFonts.acme(fontSize: 30),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add_box_sharp),
           onPressed: () {
             setState(() {
+              clicks = clicks + 1;
               generatedNumber = RandomNumberCreator.generateRandomNumber(108);
             });
-            print(generatedNumber);
+            print(clicks);
           }),
     );
   }
