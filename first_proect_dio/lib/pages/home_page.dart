db561cc91053549377bf6e4b7cbd3cf37460ab42
Alex <alexbaukartjarschel@gmail.com>
2024-02-15 19:26:49 -0300

    flex column Row

--- first_proect_dio/lib/pages/home_page.dart
+++ first_proect_dio/lib/pages/home_page.dart
@@ -23,22 +23,71 @@ class _HomePageState extends State<HomePage> {
           style: GoogleFonts.pacifico(),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              " Random nnumber $generatedNumber",
-              style: GoogleFonts.acme(fontSize: 30),
+      body: Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.green,
+              child: Text(
+                "User Actions",
+                style: GoogleFonts.acme(fontSize: 30),
+              ),
             ),
-          ),
-          Center(
-            child: Text(
-              "foi Clicado $clicks vezes",
-              style: GoogleFonts.acme(fontSize: 30),
+            Expanded(
+              child: Container(
+                color: Colors.amber,
+                child: Text(
+                  " Random nnumber $generatedNumber",
+                  style: GoogleFonts.acme(fontSize: 30),
+                ),
+              ),
             ),
-          ),
-        ],
+            Container(
+              color: Colors.teal,
+              child: Text(
+                "foi Clicado $clicks vezes",
+                style: GoogleFonts.acme(fontSize: 30),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    color: Colors.blue,
+                    child: Text(
+                      "Nome",
+                      style: GoogleFonts.acme(fontSize: 30),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    color: Colors.red,
+                    child: Text(
+                      "Alex Jarschel",
+                      style: GoogleFonts.acme(fontSize: 30),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    color: Colors.yellow,
+                    child: Text(
+                      "30",
+                      style: GoogleFonts.acme(fontSize: 30),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add_box_sharp),
